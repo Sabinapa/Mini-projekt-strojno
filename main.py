8e99c5d65be5212ab931d9be416a1593d0080436
sabina <sabina.pauric@student.um.si>
2024-01-18 15:18:00 +0100

    refactor: refactored main.py

--- main.py
+++ main.py
@@ -2,71 +2,47 @@ import os
 
 from PIL import Image, ImageDraw
 
-pot_do_slike = "abcrki.jpg"
-slika = Image.open(pot_do_slike)
+path_image = "abcrki.jpg"
+image = Image.open(path_image)
 
+width, height = image.size
+print(width, height)
 
-# Width, height
-sirina, visina = slika.size
-print(sirina, visina)
+start_x, start_y = 0, 0
+end_x, end_y = width, height
 
-start_x = 0
-start_y = 0
-end_x = sirina
-end_y = visina
+number_squares_x = 10
+number_squares_y = 20
 
-stevilo_kvadratov_x = 10
-stevilo_kvadratov_y = 20
+width_square = width // number_squares_x
+height_square = height // number_squares_y
 
-# Višina in širina kvadrata
-sirina_kvadrata = sirina // stevilo_kvadratov_x
-visina_kvadrata = visina // stevilo_kvadratov_y
+print(f"Širina kvadrata: {width_square}px")
+print(f"Višina kvadrata: {height_square}px")
 
-print(f"Širina kvadrata: {sirina_kvadrata}px")
-print(f"Višina kvadrata: {visina_kvadrata}px")
+letter = "B"
+fileName = letter
 
-crka = "A"
-# Mapa za shranjevanje izrezanih kvadratov
-mapa_izrezanih_kvadratov = crka
-
-# Prepričajte se, da mapa obstaja, če ne, jo ustvarite
-if not os.path.exists(mapa_izrezanih_kvadratov):
-    os.makedirs(mapa_izrezanih_kvadratov)
+if not os.path.exists(fileName):
+    os.makedirs(fileName)
 
 counter = 0
-# Izreži in shrani kvadrate
-for j in range(stevilo_kvadratov_y):
-    for i in range(stevilo_kvadratov_x):
-        # Izračunaj koordinate vsakega kvadrata
-        x1 = i * sirina_kvadrata
-        y1 = j * visina_kvadrata
-        x2 = x1 + sirina_kvadrata
-        y2 = y1 + visina_kvadrata
+for j in range(number_squares_y):
+    for i in range(number_squares_x):
+        x1 = i * width_square
+        y1 = j * height_square
+        x2 = x1 + width_square
+        y2 = y1 + height_square
         counter += 1
 
         if counter == 100:
-            crka = 'B'
+            letter = 'B'
 
-        # Izreži kvadrat
-        izrezan_kvadrat = slika.crop((x1, y1, x2, y2))
+        cropSquare = image.crop((x1, y1, x2, y2))
 
-        # Sestavi pot za shranjevanje
-        pot_do_izrezanega_kvadrata = f"{mapa_izrezanih_kvadratov}/{crka}{counter}.png"
+        nameOfCropS = f"{fileName}/{letter}{counter}.png"
 
-        # Shrani izrezan kvadrat
-        izrezan_kvadrat.save(pot_do_izrezanega_kvadrata)
+        cropSquare.save(nameOfCropS)
 
-'''
-# Ustvarite objekt za risanje na sliki
-risanje = ImageDraw.Draw(slika)
 
-# Narišite črto na sredini slike (kot preverjanje risanja)
-barva_crte = (255, 0, 0)  # Barva črte v RGB formatu (zelena)
-risanje.line([(0,125), (150,125)], fill=barva_crte)
-
-# Prikažite sliko
-slika.show()
-'''
-
-# Ne pozabite končati z zapiranjem slike
-slika.close()
+image.close()
